8cc3df6dfbd28d655f85f7263e06ac4f6218098d
Huang <sihong.huang@accenture.com>
2019-09-10 10:15:21 -0400

    create initial build, set up web font, set up sass for project

---
+++ my-puppies/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/fonts/roboto/Roboto-Regular.ttf';
+import './assets/fonts/roboto/Roboto-Bold.ttf';
+import './index.scss';
+import App from './components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
